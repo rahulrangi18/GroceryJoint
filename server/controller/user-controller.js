8c1b01e01439a977500b4c6521d08b0fb2ac43d8
rahulrangi18 <rahulrangi18@gmail.com>
2021-10-07 10:47:29 +0530

    JTW Auth Added

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,56 +1,50 @@
-import express from 'express';
-import User from '../model/userSchema.js';
+import express from "express";
+import User from "../model/userSchema.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-const saltRounds = 12;
 export const userSignup = async (request, response) => {
-    const { fullname, phone, email, password } = request.body;
-    try {
-        const exist = await User.findOne({ email: request.body.email});
-        // console.log(exist);
+  try {
+    const exist = await User.findOne({ email: request.body.email });
+    // console.log(exist);
 
-        if(exist) {
-            // console.log(exist);
+    if (exist) {
+      // console.log(exist);
 
-            console.log({'message':' User already exist'});
-            return response.status(401 ).json({ 'message': 'User already exist'});
-        }
-        const hash = bcrypt.hashSync(password, saltRounds);
-        const user = request.body;
-        const newUser = new User({fullname, phone, email, password:hash});
-        //password hashing
-        console.log(newUser);
-        await newUser.save();
-        response.status(200).json(`${user.fullname} has been successfully registered`);
-        console.log(`${user.fullname} has been successfully registered`);
-        
-    } catch (error) {
-        // response.send('Error: ', error.message);
-        console.log(error.message);
+      console.log({ message: " User already exist" });
+      return response.status(401).json({ message: "User already exist" });
     }
-}
-
+    const user = request.body;
+    const newUser = new User(user);
+    //password hashing
+    console.log(newUser);
+    await newUser.save();
+    response
+      .status(200)
+      .json(`${user.fullname} has been successfully registered`);
+    console.log(`${user.fullname} has been successfully registered`);
+  } catch (error) {
+    // response.send('Error: ', error.message);
+    console.log(error.message);
+  }
+};
 
 export const userLogin = async (req, res) => {
-    try {
-        const {email,password}=req.body;
-        const user = await User.findOne({ email: email});
-        console.log(user); 
-        if(user){
-            const isMatch= bcrypt.compareSync(password,user.password);
-            const token=await user.generateAuthToken();
-            console.log(token);
-            if(isMatch)
-             {
-                return res.status(200).json(`${user.fullname} has login successfully`);
-             }
-            else{
-                return res.status(400).json({error:"Invalid Details"});
-            }
-        } else {
-            return res.status(401).json('Invalid Login');
-        }
-    } catch (error) {
-        console.log(error.message);
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: req.body.email });
+    console.log(user);
+    if (user) {
+      const isMatch = await bcrypt.compare(req.body.password, user.password);
+      const token = await user.generateAuthToken();
+      console.log(token);
+      if (isMatch) {
+        return res.status(200).json(`${user.fullname} has login successfully`);
+      } else {
+        return res.status(400).json({ error: "Invalid Details" });
+      }
+    } else {
+      return res.status(401).json("Invalid Login");
     }
-}
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -3,67 +3,61 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema({
-    fullname:{
-        type: 'string',
+  fullname: {
+    type: "string",
+    required: true,
+    trim: true,
+    min: 3,
+    max: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 3,
+    max: 20,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 20,
+  },
+  phone: {
+    type: Number,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
         required: true,
-        trim: true,
-        min:3,
-        max:20,
-
+      },
     },
-    email:{
-        type: String,
-        required:true,
-        trim:true,
-        min:3,
-        max:20,
-        lowercase:true
-    },
-    password:{
-        type: String,
-        required:true,
-        min:6,
-        max:20,
-    },
-    phone:{
-        type:Number,
-    },
-    tokens:[
-        {
-            token:{
-                 type: String,
-                 required:true
-            }
-        }
-    ]
-})
+  ],
+});
 //Password Hashing
-userSchema.pre('save',async function (next) {
-    try{
-               const salt= await bcrypt.genSalt(10)
-               const hashedPassword= await bcrypt.hash(this.password,salt)
-               this.password=hashedPassword
-               next()
-    }
-    catch(error)
-      {
-          next(error)
-      }
-})
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 //token generation
-userSchema.methods.generateAuthToken = async function(){
-    try{
-         let token=jwt.sign({_id:this._id},process.env.SECRET_KEY);
-         this.tokens=this.tokens.concat({token:token});
-         await this.save();
-         return token;
-    }
-    catch(err)
-       {
-           console.log(err);
-       }
-}
-const User=mongoose.model('user',userSchema);
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const User = mongoose.model("user", userSchema);
 
 export default User;
-
