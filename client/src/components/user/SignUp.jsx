1254ee27157b0eda4ce2eb0c8436c09cf1a107e5
rahulrangi18 <rahulrangi18@gmail.com>
2021-10-28 21:06:04 +0530

    Google Auth -Almost done

--- client/src/components/user/SignUp.jsx
+++ client/src/components/user/SignUp.jsx
@@ -6,6 +6,8 @@ import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import EmailIcon from '@mui/icons-material/Email';
 import GoogleIcon from '@mui/icons-material/Google';
 import ClearIcon from '@mui/icons-material/Clear';
+import GoogleLogin from "react-google-login";
+import axios from "axios";
 import {authenticateSignup} from '../../service/api'
 
 const useStyles = makeStyles((theme) => ({
@@ -100,27 +102,79 @@ export default function SignUp({ open, setOpen, toggleSignAccount }) {
         if(!response) return console.log("response not found");
         // setAccount(signup.username)
        }
-    
+    const responseSuccessGoogle = (response) => {
+      console.log("Google Sign in was successful,here is your response");
+      console.log(response);
+      axios({
+        method: "POST",
+        url: "http://localhost:5000/api/googlelogin",
+        data: { tokenId: response.tokenId },
+      }).then((response) => {
+        console.log(response);
+      });
+    };
+    const responseErrorGoogle = (error) => {
+      console.log(error);
+      console.log("Google Sign in was unsuccessful");
+    };
 
     return (
-        <Dialog open={open} onClose={handleClose}>
-            <DialogContent className={classes.component} >
-                <Box className={classes.login}>
-                    <Box className={classes.logHeader} >
-                        <Typography style={{ fontSize: "25px"}} >SignUp</Typography>
-                        <ClearIcon onClick={()=>handleClose()} className={classes.clearIcon} />
-                    </Box>
-                    {/* <TextField onChange={(e) => onValueChange(e)} name='phone' label='Phone number' InputProps={{
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent className={classes.component}>
+          <Box className={classes.login}>
+            <Box className={classes.logHeader}>
+              <Typography style={{ fontSize: "25px" }}>SignUp</Typography>
+              <ClearIcon
+                onClick={() => handleClose()}
+                className={classes.clearIcon}
+              />
+            </Box>
+            {/* <TextField onChange={(e) => onValueChange(e)} name='phone' label='Phone number' InputProps={{
                         startAdornment: <InputAdornment position="start">kg</InputAdornment>,
                     }} /> */}
-                    <Box sx={{ display: 'flex', alignItems: 'flex-end', marginTop:"10px",flexDirection: 'column'}}>
-                        {/* <LocalPhoneIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
-                        <TextField onChange={(e)=>onValueChange(e)} id="input-with-sx" name='fullname' label="Full Name" variant="standard" style={{width: '100%',}}/>
-                        <TextField onChange={(e)=>onValueChange(e)} id="input-with-sx" name='email' label="Email" variant="standard" style={{width: '100%',marginTop: "20px"}}/>
-                        <TextField onChange={(e)=>onValueChange(e)} id="input-with-sx" name='password' label="Password" variant="standard" style={{width: '100%',marginTop: "20px"}}/>
-                        <TextField onChange={(e)=>onValueChange(e)} id="input-with-sx" name='phone' label="Phone Number" variant="standard" style={{width: '100%',marginTop: "20px"}}/>
-                    </Box>
-                    {/* <Box boxShadow={2} className={classes.search}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "flex-end",
+                marginTop: "10px",
+                flexDirection: "column",
+              }}
+            >
+              {/* <LocalPhoneIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+              <TextField
+                onChange={(e) => onValueChange(e)}
+                id="input-with-sx"
+                name="fullname"
+                label="Full Name"
+                variant="standard"
+                style={{ width: "100%" }}
+              />
+              <TextField
+                onChange={(e) => onValueChange(e)}
+                id="input-with-sx"
+                name="email"
+                label="Email"
+                variant="standard"
+                style={{ width: "100%", marginTop: "20px" }}
+              />
+              <TextField
+                onChange={(e) => onValueChange(e)}
+                id="input-with-sx"
+                name="password"
+                label="Password"
+                variant="standard"
+                style={{ width: "100%", marginTop: "20px" }}
+              />
+              <TextField
+                onChange={(e) => onValueChange(e)}
+                id="input-with-sx"
+                name="phone"
+                label="Phone Number"
+                variant="standard"
+                style={{ width: "100%", marginTop: "20px" }}
+              />
+            </Box>
+            {/* <Box boxShadow={2} className={classes.search}>
                         <div className={classes.searchIcon}>
                             <LocationOnIcon />
                         </div>
@@ -137,16 +191,50 @@ export default function SignUp({ open, setOpen, toggleSignAccount }) {
                         <Divider orientation="vertical" flexItem />
                         <SearchBar />
                     </Box> */}
-                    {/* <TextField onChange={(e) => onValueChange(e)} name='password' label='Enter Password' /> */}
-                    <Button className={classes.btnSend}  variant='contained' color="primary" onClick={()=>signupUser()} >Create Account</Button>
-                    {/* <Divider>OR</Divider> */}
-                    <Typography style={{ textAlign: 'center', marginTop: "20px"}} >or</Typography>
-                    {/* <Button className={classes.btnEmail}variant='contained' color="primary" startIcon={<EmailIcon/>} >Continue with Email</Button> */}
-                    <Button className={classes.btnEmail} variant='contained' color="primary" startIcon={<GoogleIcon/>} >Continue with Google</Button>
-                    <Divider style={{borderTop: "1px solid #000", marginTop: "20px"}} />
-                    <Typography className={classes.createAccount} onClick={()=>toggleSignAccount()}  >Already Have an account? <span style={{color:"red"}} > Log in</span></Typography>
-                </Box>
-            </DialogContent>
-        </Dialog>
-    )
+            {/* <TextField onChange={(e) => onValueChange(e)} name='password' label='Enter Password' /> */}
+            <Button
+              className={classes.btnSend}
+              variant="contained"
+              color="primary"
+              onClick={() => signupUser()}
+            >
+              Create Account
+            </Button>
+            {/* <Divider>OR</Divider> */}
+            <Typography style={{ textAlign: "center", marginTop: "20px" }}>
+              or
+            </Typography>
+            {/* <Button className={classes.btnEmail}variant='contained' color="primary" startIcon={<EmailIcon/>} >Continue with Email</Button> */}
+            <GoogleLogin
+              clientId="548736927785-l1s640me1mmgc2it65d583kbrb16ueg2.apps.googleusercontent.com"
+              render={(renderProps) => (
+                <Button
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  className={classes.btnEmail}
+                  variant="contained"
+                  color="primary"
+                  startIcon={<GoogleIcon />}
+                >
+                  Continue with Google
+                </Button>
+              )}
+              onSuccess={responseSuccessGoogle}
+              onFailure={responseErrorGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
+            <Divider
+              style={{ borderTop: "1px solid #000", marginTop: "20px" }}
+            />
+            <Typography
+              className={classes.createAccount}
+              onClick={() => toggleSignAccount()}
+            >
+              Already Have an account?{" "}
+              <span style={{ color: "red" }}> Log in</span>
+            </Typography>
+          </Box>
+        </DialogContent>
+      </Dialog>
+    );
 }
